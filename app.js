09f35ef7a7d3814a0186734ee3e3a398854f4e16
Cho, Kyungje <jkjk396@gmail.com>
2018-12-07 16:20:27 +0900

    fsdafsadf

--- app.js
+++ app.js
@@ -71,4 +71,48 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+db.query('SELECT * FROM customer WHERE fcm_token is not null;', function(error, results) {
+  if (error){
+    return false;
+  }
+
+  client_token = results[0].fcm_token;
+})
+/** 발송할 Push 메시지 내용 */
+var push_data = {
+  // 수신대상
+  to: client_token,
+  // App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
+  notification: {
+      title: "Hello Node",
+      body: "Node로 발송하는 Push 메시지 입니다.",
+      sound: "default",
+      click_action: "FCM_PLUGIN_ACTIVITY",
+      icon: "fcm_push_icon"
+  },
+  // 메시지 중요도
+  priority: "high",
+  // App 패키지 이름
+  restricted_package_name: "study.cordova.fcmclient",
+  // App에게 전달할 데이터
+  data: {
+      num1: 2000,
+      num2: 3000
+  }
+};
+
+/** 아래는 푸시메시지 발송절차 */
+var fcm = new FCM(serverKey);
+
+fcm.send(push_data, function(err, response) {
+  if (err) {
+      console.error('Push메시지 발송에 실패했습니다.');
+      console.error(err);
+      return;
+  }
+
+  console.log('Push메시지가 발송되었습니다.');
+  console.log(response);
+});
+
 module.exports = app;
