8efe99b22e0c8cf62187e65eec2ca1e9978ac74c
Cho, Kyungje <jkjk396@gmail.com>
2018-12-10 05:27:37 +0900

    ddddd

--- routes/api.js
+++ routes/api.js
@@ -52,6 +52,14 @@ var multer = require('multer')
 var hello = require('../api/hello.json')
 var db = require('../lib/db')
 var CryptoPasswd = require('../lib/passwordSecret');
+var FCM = require('fcm-node');
+
+/** Firebase(구글 개발자 사이트)에서 발급받은 서버키 */
+
+var serverKey = require('../ddib-fcm.json');
+
+/** 아래는 푸시메시지 발송절차 */
+var fcm = new FCM(serverKey);
 
 var auth = require('../lib/auth')
 
@@ -679,6 +687,38 @@ router.post('/item', passport.authenticate('jwt', { session: false }), /*upload.
       res.json(result);
       return false;
     }
+
+    var message = {};
+    db.query('SELECT * FROM wtb A INNDER JOIN customer B A.cid = B.cid WHERE A.cateid = ?;',[cateid], function(error2, results2) {
+      if (error2) {
+        res.json(result);
+        return false;
+      }
+
+      for (var i = 0; i < results2.length; i++) {
+        message = {
+            // 수신대상
+            to: results2[i].fcm_token,
+            // App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
+            notification: {
+                title: results2[i].name + "님!",
+                body: "삽니다!에 등록하신 카테고리로 새 음식이 올라왔어요!",
+                
+            },
+        };
+
+        fcm.send(message, function(err, response) {
+            if (err) {
+                console.error('Push메시지 발송에 실패했습니다.');
+                console.error(err);
+                return false;
+            }
+        
+            console.log('Push메시지가 발송되었습니다.');
+            console.log(response);
+        });
+      }
+    })
 
     result['success'] = true;
     res.json(result);
